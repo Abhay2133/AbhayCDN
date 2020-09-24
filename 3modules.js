ab626a106529f43a1e357b02132d4d6b408b4697
Abhay2133 <63441094+Abhay2133@users.noreply.github.com>
2020-09-24 18:17:34 +0530

    Create 3modules.js

---
+++ 3modules.js
@@ -0,0 +1,34 @@
+const se = (key, val) => {
+        switch (key) {
+            
+ case 'i': return document.getElementById(val);
+    break;
+ case 'c': return document.getElementsByClassName(val);
+    break;
+ case 't': return document.getElementsByTagName(val);
+    break;
+    
+        }
+        
+        return 0;
+    }
+    
+function ae(el,event, method, params, callback)
+{
+    el.addEventListener(event, function (){
+        method(this, params);
+    });
+    
+    if(callback)
+    callback();
+}
+
+function css(el, props, callback)
+{
+
+    for (let i in props)
+        el.style[i] = props[i];
+
+    if (callback)
+        callback();
+}
